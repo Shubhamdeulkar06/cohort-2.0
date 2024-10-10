61b74bd087ad2c9624bff505c1e0a95c365644ae
Shubhamdeulkar06 <shuboy0868@gmail.com>
2024-10-10 16:09:56 +0530

    week 3, validation without using middleware

---
+++ week-3/3.1/index.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const app = express();
+// app.use(express.json);
+
+app.get("/health", (req, res) => {
+  const username = req.headers.username;
+  const password = req.headers.password;
+  const kidneyId = req.query.kidneyId;
+// this is dum way of validation
+  if (username != "shubham" || password != "pass") {
+    res.status(400).json({
+      msg: "somthings is wrong with your inputs",
+    });
+    return;
+  }
+  // do some standard logic here
+  if (kidneyId != 1 && kidneyId != 2) {
+    res.status(400).json({
+      msg: "somthings is wrong with your id",
+    });
+    return;
+  }
+  res.json({
+    msg: "Your kidney is fine",
+  });
+});
+
+app.listen(3000, () => {
+  console.log("listening.....");
+});
